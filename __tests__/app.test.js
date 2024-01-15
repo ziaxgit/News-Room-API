3bf2857b07303445b2f66beb46cd63b082f5fcdc
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-01-15 22:25:56 +0000

    add further tests for task 2 & 3

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,34 +8,64 @@ const endpointJson = require("../endpoints.json");
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
-describe("/api/topics", () => {
-  test("GET:200 returns an array of topic objects with slug and description properties", () => {
+// for any path that's not in endpoint
+describe("Invalid path", () => {
+  test("status:404 returns appropriate message for any invalid path", () => {
+    return request(app)
+      .get("/not-a-valid-path")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.message).toBe("Not Found");
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("status:200 returns an array of topics", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
-      .then((response) => {
-        response.body.topics.forEach((topic) => {
+      .then(({ body }) => {
+        expect(Array.isArray(body.topics)).toBe(true);
+      });
+  });
+  test("status:200 returns an array with 3 objects", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.topics.length).toBe(3);
+      });
+  });
+  test("status:200 returns an array of topic objects with slug and description properties", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.topics)).toBe(true);
+        expect(body.topics.length).toBe(3);
+        body.topics.forEach((topic) => {
           expect(typeof topic.description).toBe("string");
           expect(typeof topic.slug).toBe("string");
         });
       });
   });
 });
-describe("/api", () => {
-  test("GET:200 returns an object describing all the available API endpoints", () => {
+describe("GET /api", () => {
+  test("status:200 returns an object", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(typeof body.allEndpoints).toBe("object");
+      });
+  });
+  test("status:200 returns an object describing all the available API endpoints", () => {
     return request(app)
       .get("/api")
       .expect(200)
       .then(({ body }) => {
         expect(body.allEndpoints).toEqual(endpointJson);
-      });
-  });
-  test("GET:404 returns appropriate message for any invalid path", () => {
-    return request(app)
-      .get("/not-api")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.message).toBe("Not Found");
       });
   });
 });
--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const app = express();
-
-const { getAllTopics } = require("./controllers/topics-controller");
 const getAllEndpoints = require("./controllers/endpoint-controller");
+const { getAllTopics } = require("./controllers/topics-controller");
 
 app.get("/api/topics", getAllTopics);
 app.get("/api", getAllEndpoints);
 
-app.use((req, res) => {
-  // if not path matches give default error
+app.all("/*", (req, res) => {
+  // if no path matches give default error
   res.status(404).send({ message: "Not Found" });
 });
 
