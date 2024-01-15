da436f0c19c7f694ed7d41aa809ee318d6c7b933
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-01-15 13:42:42 +0000

    initial setup

---
+++ __tests__/app.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const seed = require("../db/seeds/seed");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
