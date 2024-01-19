c016c3474494189ca6e737b13d166ee99678a2af
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-01-19 19:57:37 +0000

    undo changing users data

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,37 +1,37 @@
 module.exports = [
   {
-    username: "LunarExplorer77",
-    name: "Aria Starlight",
+    username: "tickle122",
+    name: "Tom Tickle",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
   },
   {
-    username: "BreezyAdventurer",
-    name: "Caspian Windwhisper",
+    username: "grumpy19",
+    name: "Paul Grump",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
   },
   {
-    username: "JoyfulSerenader",
-    name: "Luna Harmony",
+    username: "happyamy2016",
+    name: "Amy Happy",
     avatar_url:
       "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
   },
   {
-    username: "DynamicVoyager",
-    name: "Orion Dynamo",
+    username: "cooljmessy",
+    name: "Peter Messy",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
   },
   {
-    username: "GlowingExplorer22",
-    name: "Nova Radiance",
+    username: "weegembump",
+    name: "Gemma Bump",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
   },
   {
-    username: "MelodicCosmonaut",
-    name: "Lyra Stellaris",
+    username: "jessjelly",
+    name: "Jess Jelly",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
   },
