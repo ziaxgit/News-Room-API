ff45529fa9cc505e01aa01d1a794022e5d6b5707
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-01-19 19:42:02 +0000

    replace users dev data

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -1,38 +1,38 @@
 module.exports = [
   {
-    username: 'tickle122',
-    name: 'Tom Tickle',
+    username: "LunarExplorer77",
+    name: "Aria Starlight",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953'
+      "https://vignette.wikia.nocookie.net/mrmen/images/d/d6/Mr-Tickle-9a.png/revision/latest?cb=20180127221953",
   },
   {
-    username: 'grumpy19',
-    name: 'Paul Grump',
+    username: "BreezyAdventurer",
+    name: "Caspian Windwhisper",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013'
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/78/Mr-Grumpy-3A.PNG/revision/latest?cb=20170707233013",
   },
   {
-    username: 'happyamy2016',
-    name: 'Amy Happy',
+    username: "JoyfulSerenader",
+    name: "Luna Harmony",
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+      "https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729",
   },
   {
-    username: 'cooljmessy',
-    name: 'Peter Messy',
+    username: "DynamicVoyager",
+    name: "Orion Dynamo",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002'
+      "https://vignette.wikia.nocookie.net/mrmen/images/1/1a/MR_MESSY_4A.jpg/revision/latest/scale-to-width-down/250?cb=20170730171002",
   },
   {
-    username: 'weegembump',
-    name: 'Gemma Bump',
+    username: "GlowingExplorer22",
+    name: "Nova Radiance",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553'
+      "https://vignette.wikia.nocookie.net/mrmen/images/7/7e/MrMen-Bump.png/revision/latest?cb=20180123225553",
   },
   {
-    username: 'jessjelly',
-    name: 'Jess Jelly',
+    username: "MelodicCosmonaut",
+    name: "Lyra Stellaris",
     avatar_url:
-      'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141'
-  }
+      "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
+  },
 ];
