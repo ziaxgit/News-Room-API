f9a435d68385bc554e2b7485f5aa5e19aa31b1af
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-02-05 10:08:29 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const apiRouter = require("./routers/api-router");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
@@ -10,12 +12,10 @@ app.use(errorHandler);
 
 app.all("/*", (req, res) => {
   // if no path matches give default error
-  res
-    .status(404)
-    .send({
-      message:
-        "Invalid endpoint. Use /api/ to find a list of available endpoints",
-    });
+  res.status(404).send({
+    message:
+      "Invalid endpoint. Use /api/ to find a list of available endpoints",
+  });
 });
 
 module.exports = app;
